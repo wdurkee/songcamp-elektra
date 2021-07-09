a907fef5ef77b7379bcf98499de3fe998c1091af
Kevin Davis <thekidnamedkd@gmail.com>
2021-07-09 19:09:44 +0200

    change indents on mailchimpform

--- src/MailChimpForm.js
+++ src/MailChimpForm.js
@@ -7,28 +7,28 @@ function MailChimpForm() {
 
     return (
         <div>
-        <Mailchimp 
-            className="chimpForms"
-            action="https://band.us6.list-manage.com/subscribe/post?u=f4851db0998383af4e573916e&id=9562a75ed0"
-            fields={[
-                {
-                    name: 'EMAIL',
-                    placeholder: 'email...',
-                    type: 'email',
-                    required: true
+            <Mailchimp 
+                className="chimpForms"
+                action="https://band.us6.list-manage.com/subscribe/post?u=f4851db0998383af4e573916e&id=9562a75ed0"
+                fields={[
+                    {
+                        name: 'EMAIL',
+                        placeholder: 'email...',
+                        type: 'email',
+                        required: true
+                    }
+                ]}
+                messages = {
+                    {
+                        sending: "SENDING...",
+                        success: "INSTRUCTIONS LANDING SOON",
+                        error: "ERROR",
+                        empty: "ENTER EMAIL",
+                        duplicate: "ALREADY ON THE LIST",
+                        button: "ENTER"
+                    }
                 }
-            ]}
-            messages = {
-                {
-                    sending: "SENDING...",
-                    success: "INSTRUCTIONS LANDING SOON",
-                    error: "ERROR",
-                    empty: "ENTER EMAIL",
-                    duplicate: "ALREADY ON THE LIST",
-                    button: "ENTER"
-                }
-            }
-        />
+            />
         </div>
     )
 }
