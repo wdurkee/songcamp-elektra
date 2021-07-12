6ffe5dc92478c6fd65e6b927298dfe1c0ff582f9
Kevin Davis <thekidnamedkd@gmail.com>
2021-07-12 21:52:31 +0200

    increase logo size

--- src/ElektraSatellite.js
+++ src/ElektraSatellite.js
@@ -4,7 +4,7 @@ import Logo from "./ELEKTRA.png"
 function ElektraSatellite() {
     return (
         <div>
-            <img src={Logo} alt="Elektra Logo" style={{ marginTop: "40px", maxWidth: "40px", height: "auto" }}/>
+            <img src={Logo} alt="Elektra Logo" style={{ marginTop: "60px", marginBottom: "10px", maxWidth: "60px", height: "auto" }}/>
         </div>
     )
 }
