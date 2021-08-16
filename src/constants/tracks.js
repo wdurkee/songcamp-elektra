f628a118cd4454f68e02d67f82bde56f2767c8a6
Will Durkee <william.w.durkee@gmail.com>
2021-08-16 12:53:38 -0700

    adding nfts

--- src/constants/tracks.js
+++ src/constants/tracks.js
@@ -1,32 +1,32 @@
 export const track1 = {
-    tokenId: 664,
+    tokenId: 4554,
     audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeigmxqgsvmaken72wd54flbfcs2x4iele3tes2j2r2hm7oawuymfbu',
     imageSrc: 'https://i.imgur.com/KkI87jw.jpg',
     title: 'Outer Boroughs',
     artist: 'Finch-3S',
-    artistUrl: 'http://beta.catalog.works/',
-    catalogUrl: 'http://beta.catalog.works/',
+    artistUrl: 'https://zora.co/finch3s',
+    catalogUrl: 'https://zora.co/finch3s/4554',
     partyBidUrl: 'https://www.partybid.app/'
 }
 
 export const track2 = {
-    tokenId: 4318, // 4543 not kicked off nft
+    tokenId: 4556, // 4543 not kicked off nft
     audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeicbnbkzehly7q267kna6fuw3r4y7i7kpdh3maghbn2b2lp6pxqoxa',
     imageSrc: 'https://i.imgur.com/UmDq5t8.jpg',
     title: 'Crash Desire',
     artist: 'AME RA',
-    artistUrl: 'http://beta.catalog.works/',
-    catalogUrl: 'http://beta.catalog.works/',
+    artistUrl: 'https://zora.co/amera',
+    catalogUrl: 'https://zora.co/amera/4556',
     partyBidUrl: 'https://www.partybid.app/'
 }
 
 export const track3 = {
-    tokenId: 4318,
+    tokenId: 4555,
     audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeiaax2mz54fdfklbcdevv2mmch6pqzyljuggytb2rghpn4xd44t3p4',
     imageSrc: 'https://i.imgur.com/45GqLmV.jpg',
     title: "Don't Worry",
     artist: 'Water Signs',
-    artistUrl: 'http://beta.catalog.works/',
-    catalogUrl: 'http://beta.catalog.works/',
+    artistUrl: 'https://zora.co/watersigns',
+    catalogUrl: 'https://zora.co/watersigns/4555',
     partyBidUrl: 'https://www.partybid.app/'
 }
