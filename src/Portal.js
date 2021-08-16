4c1154199e0f75f9fc64a60f343c1859ce9f239c
Will Durkee <william.w.durkee@gmail.com>
2021-08-16 09:58:33 -0700

    copy updated

--- src/Portal.js
+++ src/Portal.js
@@ -17,7 +17,7 @@ function Portal() {
         </Navbar.Brand>
       </Navbar>
       <div className="section auction" >
-        <img src="https://i.imgur.com/REab9mL.png" alt="The Elektra Songs Auction" className="title-image" />
+        <h1 className="section-title">The Elektra Songs Auction</h1>
         <AudioPlayer />
         <div className='button-container'>
           <a className='portal-button party-button button' href='https://www.partybid.app/' target="_blank" rel="noreferrer">
@@ -28,15 +28,24 @@ function Portal() {
 
       <div className="capsule-container">
         <video preload="yes" muted autoPlay playsInline loop className="capsule-video" >
-          <source src="https://storageapi.fleek.co/wdurkee-team-bucket/cropped-capsule.mp4" type="video/mp4" />
+          <source src="https://ipfs.fleek.co/ipfs/bafybeibjeowdroq3ox2bybs7g7mpe5hhvqqcndxkiticfxnxtc3qbmgwwu" type="video/mp4" />
           {/* <source src="landscan.webm" type="video/webm" /> */}
           Your browser does not support the video tag.
         </video>
       </div>
 
       <div className="story-content">
-        <p className="story-text" >Songcamp Elektra is an 8-week songwriting and art creation camp made up of musicians, visual artists, developers and strategists. Together we have collectively created Elektra — an interactive story and world with music at its core.</p>
-        <p className="story-text" >In the world of Elektra, music is energy. Music is the fuel, electricity, and light of the world. Once healthy and thriving, Elektra has since turned desolate and barren. These 3 songs were created to help save this dying planet, and bring her back to health.</p>
+        <p className="story-text welcome-text" >Welcome to the Elektra Songs Auction!</p>
+        <p className="story-text">100% of the proceeds from these auctions will go to the artists who created these songs and artwork, as well as those who have contributed to the wider Elektra project. See detailed splits <span>
+          <a className="learn-more-link" href='https://songcamp.mirror.xyz/' target="_blank" rel="noreferrer">
+            here
+          </a>
+        </span>. </p>
+        <p className="story-text" >Collecting one of these 1/1 Catalog NFTs also includes:</p>
+        <ul>
+          <li className="bullet">A gift of 1000 $ELEKTRA</li>
+          <li className="bullet">One (1) invitation to Songcamp's Patron House -- a dedicated space for patrons of 1/1 Songcamp NFTs to receive deep + intimate access into the growing Songcamp community and activities.</li>
+        </ul>
         <p className="story-text" >After the camp closes, the project will exit to its community and evolve into the Elektra DAO. The Elektra DAO will steward the continued development and distribution of the Elektra story, game and artwork. The Elektra DAO is scheduled to activate at the beginning of September 2021.</p>
 
         {/* <p className="story-text"><span style={{ fontStyle: "italic" }}>Will you accept your mission?</span></p> */}
@@ -48,7 +57,7 @@ function Portal() {
           Your browser does not support the video tag.
         </video>
         <div className="video-overlay">
-          <div className='button-container'>
+          {/* <div className='button-container'>
             <a className='portal-button' href='https://zora.co/songcamp' target="_blank" rel="noreferrer">
               SEE THE ARTWORK
             </a>
@@ -57,20 +66,29 @@ function Portal() {
             <a className='portal-button' href='https://discord.gg/v6hj82yeaE' target="_blank" rel="noreferrer">
               ENTER THE PORTAL
             </a>
-          </div>
+          </div> */}
           <MailChimpForm />
         </div>
       </div>
       <div className="section about" >
         <h1 className="section-title about-title">About Elektra</h1>
-        <p className="story-text">Songcamp Elektra is made up of 42 musicians, visual artists, developers and strategists. Together we are building “Elektra” — an interactive choose-your-own-adventure web3 game with music at its core.</p>
-        <p className="story-text">To play the game, you need to secure a Portal Ticket NFT in the crowdfund below.</p>
-        <p className="story-text">As this project evolves, the lines between creator and audience will blur. Game builders will play the game they are building; and game players will build the game they are playing. The $ELEKTRA token will come to establish the contribution graph of this project, and once the camp is finished — the ElektraDAO will be born.</p>
+        <p className="story-text">Songcamp Elektra is an 8-week songwriting and art creation camp made up of musicians, visual artists, developers and strategists. Together we have collectively created Elektra — an interactive story and world with music at its core.</p>
+        <p className="story-text">In the world of Elektra, music is energy. Music is the fuel, electricity, and light of the world. Once healthy and thriving, Elektra has since turned desolate and barren. These 3 songs have been created to help save this dying planet, and bring her back to health.</p>
+        <p className="story-text">After the camp closes, the project will <span style={{ fontStyle: "italic" }}>exit to its community</span> and evolve into the Elektra DAO.</p>
+        <p className="story-text">The Elektra DAO will steward the continued development and distribution of the Elektra story, game and artwork. The $ELEKTRA token will act as the governing token to make these collective decisions.</p>
+        <p className="story-text">The Elektra DAO is scheduled to activate at the beginning of September 2021.</p>
+
         <p className="story-text learn-more">Learn More: <span><a className="learn-more-link" href='https://songcamp.mirror.xyz/' target="_blank" rel="noreferrer">
           https://songcamp.mirror.xyz/
         </a>
         </span>
         </p>
+        <div className='button-container'>
+          <a className='portal-button' href='https://zora.co/songcamp' target="_blank" rel="noreferrer">
+            SEE THE ARTWORK
+          </a>
+        </div>
+
 
       </div>
       <div className="powerBottom">
--- src/constants/tracks.js
+++ src/constants/tracks.js
@@ -1,6 +1,6 @@
 export const track1 = {
     tokenId: 664,
-    audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeibz657pufneyfaooafdez2vpktxbnoyztdnj6leszefb4p2go2yni',
+    audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeigmxqgsvmaken72wd54flbfcs2x4iele3tes2j2r2hm7oawuymfbu',
     imageSrc: 'https://i.imgur.com/KkI87jw.jpg',
     title: 'Outer Boroughs',
     artist: 'Finch 3s',
@@ -14,7 +14,7 @@ export const track2 = {
     audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeicbnbkzehly7q267kna6fuw3r4y7i7kpdh3maghbn2b2lp6pxqoxa',
     imageSrc: 'https://i.imgur.com/VT9wr4Z.jpg',
     title: 'Crash Desire',
-    artist: 'Ame Ra',
+    artist: 'AME RA',
     artistUrl: 'http://beta.catalog.works/',
     catalogUrl: '',
     partyBidUrl: ''
@@ -22,7 +22,7 @@ export const track2 = {
 
 export const track3 = {
     tokenId: 4372,
-    audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeidq5atifv5vjhzajgfxnvd45xqjdesimrcnapiwak7romia54ghxy',
+    audioSrc: 'https://ipfs.fleek.co/ipfs/bafybeiaax2mz54fdfklbcdevv2mmch6pqzyljuggytb2rghpn4xd44t3p4',
     imageSrc: 'https://i.imgur.com/45GqLmV.jpg',
     title: "Don't Worry",
     artist: 'Water Signs',
