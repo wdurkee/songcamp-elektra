7528457bd71c847466e3ba0a59c18e5bd65d234f
Will Durkee <william.w.durkee@gmail.com>
2023-03-01 21:55:15 -0800

    links

--- src/constants/tracks.js
+++ src/constants/tracks.js
@@ -4,8 +4,8 @@ export const track1 = {
     imageSrc: 'https://i.imgur.com/KkI87jw.jpg',
     title: 'Outer Boroughs',
     artist: 'Finch-3S',
-    artistUrl: 'https://zora.co/finch3s',
-    catalogUrl: 'https://zora.co/finch3s/4554',
+    artistUrl: 'https://market.zora.co/finch3s',
+    catalogUrl: 'https://market.zora.co/collections/0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7/4554',
     partyBidUrl: 'https://www.partybid.app/party/0xa93cbb8658578C09E7aDA3f9d989C2E1B2026342'
 }
 
@@ -15,8 +15,8 @@ export const track2 = {
     imageSrc: 'https://i.imgur.com/UmDq5t8.jpg',
     title: 'Crash Desire',
     artist: 'AME RA',
-    artistUrl: 'https://zora.co/amera',
-    catalogUrl: 'https://zora.co/amera/4556',
+    artistUrl: 'https://market.zora.co/amera',
+    catalogUrl: 'https://market.zora.co/collections/0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7/4556',
     partyBidUrl: 'https://www.partybid.app/party/0x774793575231745674918AA96700486Ab8e74756'
 }
 
@@ -26,7 +26,7 @@ export const track3 = {
     imageSrc: 'https://i.imgur.com/45GqLmV.jpg',
     title: "Don't Worry",
     artist: 'Water Signs',
-    artistUrl: 'https://zora.co/watersigns',
-    catalogUrl: 'https://zora.co/watersigns/4555',
+    artistUrl: 'https://market.zora.co/watersigns',
+    catalogUrl: 'https://market.zora.co/collections/0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7/4555',
     partyBidUrl: 'https://www.partybid.app/party/0x569Eb19845ccB4a536A46b601752000f42D71B96'
 }
